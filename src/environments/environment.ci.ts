d25ecc6fce2b8da04878c8a9255405b91fbc83d7
Kobi Hari <kobi2294@yahoo.com>
2025-08-17 17:59:29 +0300

    github action

---
+++ src/environments/environment.ci.ts
@@ -0,0 +1,3 @@
+export const environment = JSON.parse(
+    process.env['NG_ENVIRONMENT']!
+);
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  api: {
+    firebaseConfig: {
+      apiKey: 'AIzaSyChThmAwa_1YsnaaxYqRsgHsfUKpsVySH4',
+      authDomain: 'ai-demo-f9abe.firebaseapp.com',
+      projectId: 'ai-demo-f9abe',
+      storageBucket: 'ai-demo-f9abe.firebasestorage.app',
+      messagingSenderId: '37077028461',
+      appId: '1:37077028461:web:24bc97ddc8d22039397964',
+    },
+  },
+  production: false,
+  region: 'me-west1',
+  title: 'development-mode',
+};
